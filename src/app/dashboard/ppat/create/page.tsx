6319d40b7a4314c3f96e78996898c8ccc2c58961
dikobagda <dikobagda@gmail.com>
2026-05-17 16:49:07 +0700

    fixing error

--- src/app/dashboard/ppat/create/page.tsx
+++ src/app/dashboard/ppat/create/page.tsx
@@ -29,6 +29,7 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Badge } from "@/components/ui/badge";
 import MapPicker from "@/components/MapPicker";
 import {
   Dialog,
